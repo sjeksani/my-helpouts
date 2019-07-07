f75a0b095ddd21f0ebc42326d305a5064119383b
Sharath Jeksani <admin@Admins-MacBook-Pro.local>
2019-07-07 13:37:01 -0700

    added angular material library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {AuthorService} from './services/authors.service';
 import {MyHelpoutsTobBarComponent} from './components/topbar.component/topbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import {MyHelpoutsTobBarComponent} from './components/topbar.component/topbar.co
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [AuthorService],
   bootstrap: [AppComponent]
