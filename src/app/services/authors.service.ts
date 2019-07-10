4b51a5daceea02131f017f9a3ca1cfb2be0293ef
Sharath Jeksani <admin@Admins-MacBook-Pro.local>
2019-07-09 17:54:00 -0700

    modified service file

--- src/app/services/authors.service.ts
+++ src/app/services/authors.service.ts
@@ -1,29 +1,35 @@
 
-// import { Http } from '@angular/http';
+import { Http } from '@angular/http';
+import {map} from 'rxjs/operators';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class AuthorService {
 
-    getAuthors() {
-        return [
-                    {
-                        "id": 1,
-                        "name": "Jonathan Murphy",
-                        "email": "Tristin83@gmail.com",
-                        "phone": "001-904-3992",
-                        "city": "Ericland",
-                        "country": "Saint Barthelemy",
-                        "title": "Global Directives Consultant"
-                    }
-                ]
-        
+    constructor(http: Http) {
+        http.get("http://jsonplaceholder.typicode.com/users")
+        .pipe(map(res => res.json()))
+        .subscribe((res:Response) => res);
+
     }
 
-    // getData(http: Http) {
-        // this.http.get("http://jsonplaceholder.typicode.com/users").map(
-        //    (response) ⇒ response.json()
-        // ).subscribe(
-        //    (data) ⇒ {this.displaydata(data);}
-        // )
-   
+    displaydata(data) {
+        console.log("hello sharath:", data);
+        return data;
+    }
+
+    // getAuthors() {
+    //     return [
+    //                 {
+    //                     "id": 1,
+    //                     "name": "Jonathan Murphy",
+    //                     "email": "Tristin83@gmail.com",
+    //                     "phone": "001-904-3992",
+    //                     "city": "Ericland",
+    //                     "country": "Saint Barthelemy",
+    //                     "title": "Global Directives Consultant"
+    //                 }
+    //             ]
     // }
+
 }
